af495c6cff34ebafea9350fc9d160258462fd3a7
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-20 00:55:01 +0330

    fix: make users view file more tidy

--- users/views.py
+++ users/views.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions, generics
-from users import serializers
-from users.models import User
+from users import serializers, models
 
 
 class CustomerList(generics.ListAPIView):
@@ -8,7 +7,7 @@ class CustomerList(generics.ListAPIView):
     :returns list of customers to the admin user
     """
     # TODO: implement filter
-    queryset = User.objects.all()
+    queryset = models.User.objects.all()
     serializer_class = serializers.UserListSerializer
     permission_classes = [permissions.IsAdminUser]
 
@@ -17,6 +16,6 @@ class CustomerCreation(generics.CreateAPIView):
     """
     Creates customer and :returns customer object to admin user
     """
-    queryset = User.objects.all()
+    queryset = models.User.objects.all()
     serializer_class = serializers.UserCreateSerializer
     permission_classes = [permissions.IsAdminUser]
