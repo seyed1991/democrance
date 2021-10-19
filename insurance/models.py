6db233ca2036b33a07e866b8158f1fe3832d77de
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-20 00:53:36 +0330

    fix: add default values for Policy fields

--- insurance/models.py
+++ insurance/models.py
@@ -10,13 +10,14 @@ class Policy(models.Model):
 
     class StateChoices(models.IntegerChoices):
         New = 1
-        Quoted = 2
+        Quoted = 2  # Accepted
         Active = 3
 
     policy_type = models.IntegerField(choices=PolicyTypes.choices, verbose_name=_('Policy Type'), db_index=True)
-    premium = models.IntegerField(_('Premium'))
-    cover = models.IntegerField(_('Cover'))
-    state = models.IntegerField(choices=StateChoices.choices, verbose_name=_('Policy State'), db_index=True)
+    premium = models.IntegerField(_('Premium'), default=100)
+    cover = models.IntegerField(_('Cover'), default=1000)
+    state = models.IntegerField(choices=StateChoices.choices, verbose_name=_('Policy State'), db_index=True,
+                                default=StateChoices.New)
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='policies',
                                  db_index=True)
 
