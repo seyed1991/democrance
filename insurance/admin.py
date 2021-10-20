01f7a83ba1abe5d4f0a0395b2f3237a1bf11eac2
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-20 17:12:53 +0330

    feat: add policy history model and related signal handler

--- insurance/admin.py
+++ insurance/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from insurance.models import Policy
+from insurance.models import Policy, PolicyHistory
 
 
 @admin.register(Policy)
 class PolicyAdmin(admin.ModelAdmin):
     list_display = ('customer', 'state', 'policy_type')
     list_filter = ('policy_type', 'state')
+
+
+@admin.register(PolicyHistory)
+class PolicyHistoryAdmin(admin.ModelAdmin):
+    list_display = ('policy', 'action', 'action_datetime')
+    list_filter = ('action',)
--- insurance/models.py
+++ insurance/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as _
 
 
@@ -24,6 +25,39 @@ class Policy(models.Model):
     class Meta:
         verbose_name_plural = 'Policies'
 
+    def __str__(self):
+        return f'{self.customer} - {self.get_policy_type_display()}'
+
     @classmethod
     def get_choice_by_name(cls, choices, name):
         return next((item for item in choices if item.name == name), None)
+
+
+class PolicyHistory(models.Model):
+
+    class ActionTypes(models.IntegerChoices):
+        creation = 1
+        quotation = 2  # accepted by customer: new -> quote
+        activation = 3  # quote -> active
+
+    policy = models.ForeignKey(Policy, on_delete=models.CASCADE, related_name='history')
+    action = models.IntegerField(choices=ActionTypes.choices)
+    action_datetime = models.DateTimeField(auto_now=True)
+
+
+@receiver(models.signals.post_save, sender=Policy, dispatch_uid="add_policy_action_history")
+def save_policy_history_handler(sender, instance, created, **kwargs):
+    """
+    This handler is used to save policy history objects after Policy save.
+    """
+    action = None
+    if created:
+        action = PolicyHistory.ActionTypes.creation
+    elif instance.state == Policy.StateChoices.accepted:
+        action = PolicyHistory.ActionTypes.quotation
+    elif instance.state == Policy.StateChoices.active:
+        action = PolicyHistory.ActionTypes.activation
+    if not action:
+        return
+    history = PolicyHistory(policy=instance, action=action)
+    history.save()
