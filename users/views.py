d32ec3a55bf5622840143bec588c98b78d5599c7
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-20 23:47:57 +0330

    feat: add name and dob filters for customers list api endpoint

--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
+import datetime
+from django.db.models import Value
+from django.db.models.functions import Concat
 from rest_framework import permissions, generics
+
 from users import serializers, models
 
 
@@ -6,10 +10,18 @@ class CustomerList(generics.ListAPIView):
     """
     :returns list of customers to the admin user
     """
-    # TODO: implement filter
-    queryset = models.User.objects.all()
     serializer_class = serializers.UserListSerializer
     permission_classes = [permissions.IsAdminUser]
+
+    def get_queryset(self):
+        queryset = models.User.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name')).all()
+        name = self.request.query_params.get('name')
+        if name:
+            queryset = queryset.filter(full_name__icontains=name)
+        date_of_birth = self.request.query_params.get('dob')
+        if date_of_birth:
+            queryset = queryset.filter(date_of_birth=datetime.datetime.strptime(date_of_birth, "%d-%m-%Y").date())
+        return queryset
 
 
 class CustomerCreation(generics.CreateAPIView):
