e91aef40268203d2a072ddcbcb4cee783df2dc49
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-20 14:46:57 +0330

    fix: modify representation of Policy `type` and `status` fields

--- insurance/serializers.py
+++ insurance/serializers.py
@@ -3,6 +3,9 @@ from rest_framework import serializers
 
 
 class PolicySerializer(serializers.ModelSerializer):
+    type = serializers.CharField(source='get_policy_type_display')
+    status = serializers.CharField(source='get_state_display')
+
     class Meta:
         model = Policy
         fields = '__all__'
