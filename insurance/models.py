40de70f66d022ca0f6c22e0dd03a3091c254c897
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-20 14:22:17 +0330

    fix: minor change in Policy model choices for easier access

--- insurance/models.py
+++ insurance/models.py
@@ -5,21 +5,25 @@ from django.utils.translation import gettext_lazy as _
 
 class Policy(models.Model):
     class PolicyTypes(models.IntegerChoices):
-        PERSONAL_ACCIDENT = 1
-        EARTHQUAKE = 2
+        personal_accident = 1
+        earthquake = 2
 
     class StateChoices(models.IntegerChoices):
-        New = 1
-        Quoted = 2  # Accepted
-        Active = 3
+        new = 1
+        accepted = 2  # Quoted
+        active = 3
 
     policy_type = models.IntegerField(choices=PolicyTypes.choices, verbose_name=_('Policy Type'), db_index=True)
     premium = models.IntegerField(_('Premium'), default=100)
     cover = models.IntegerField(_('Cover'), default=1000)
     state = models.IntegerField(choices=StateChoices.choices, verbose_name=_('Policy State'), db_index=True,
-                                default=StateChoices.New)
+                                default=StateChoices.new)
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='policies',
                                  db_index=True)
 
     class Meta:
         verbose_name_plural = 'Policies'
+
+    @classmethod
+    def get_choice_by_name(cls, choices, name):
+        return next((item for item in choices if item.name == name), None)
