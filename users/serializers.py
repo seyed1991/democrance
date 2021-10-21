edd275e47e60ca7d7950f614bc111118d192c4fe
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-21 13:07:12 +0330

    fix: accept 'dob' in customer creation api instead of 'date_of_birth'

--- users/serializers.py
+++ users/serializers.py
@@ -14,6 +14,13 @@ class UserCreateSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'first_name', 'last_name', 'date_of_birth']
         read_only_fields = ['id', 'username']
 
+    def to_internal_value(self, data):
+        if not data.get('date_of_birth'):
+            # manage `date_of_birth` field in request data
+            data['date_of_birth'] = data['dob']
+
+        return super(UserCreateSerializer, self).to_internal_value(data)
+
     def create(self, validated_data):
         user = User(username=User.generate_username(validated_data), **validated_data)
         user.set_password(User.generate_password(validated_data))
