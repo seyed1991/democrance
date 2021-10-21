7e8b5296bb3a93abcc69be2bead424ec9465691c
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-21 13:08:25 +0330

    feat: add customer creation test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+
+
+class CustomerCreationTests(APITestCase):
+    fixtures = ["admin_user.json"]
+
+    def test_create_customer(self):
+        """
+        Test customer creation.
+        """
+        url = '/api/v1/create_customer/'
+        data = {
+            'first_name': 'Damian',
+            'last_name': 'Dimmich',
+            'dob': '25-06-1991',
+        }
+        admin_user = User.objects.get(id=1)
+        self.client.force_login(admin_user)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)  # admin user already exists
+        self.assertEqual(User.objects.get(username='dimmich_damian').first_name, 'Damian')
