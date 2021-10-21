f19542f7a2f58024b9260ef08014282660e38b55
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-21 13:26:40 +0330

    feat: complete policy process tests

--- insurance/tests.py
+++ insurance/tests.py
@@ -13,19 +13,73 @@ class PolicyProcessTests(APITestCase):
         Test quote creation.
         """
         url = '/api/v1/quote/'
-        data = {
-            'customer_id': 1,
-            'type': 'personal-accident',
-        }
         admin_user = User.objects.get(id=1)
         self.client.force_login(admin_user)
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(url, {'customer_id': 1, 'type': 'personal-accident', }, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Policy.objects.count(), 1)  # admin user already exists
+        self.assertEqual(Policy.objects.count(), 1)
         policy = Policy.objects.get()
         self.assertEqual(policy.customer_id, 1)
         self.assertEqual(policy.policy_type, Policy.PolicyTypes.personal_accident)
         self.assertEqual(policy.state, Policy.StateChoices.new)
         self.assertEqual(
             PolicyHistory.objects.filter(policy=policy, action=PolicyHistory.ActionTypes.creation).count(), 1
+        )
+
+    def test_quote_acceptance(self):
+        """
+        Test quote getting accepted.
+        """
+        url = '/api/v1/quote/'
+        admin_user = User.objects.get(id=1)
+        self.client.force_login(admin_user)
+        quote = Policy(
+            policy_type=Policy.PolicyTypes.personal_accident,
+            customer=admin_user
+        )
+        quote.save()
+        response = self.client.post(url, {'quote_id': 1, 'status': 'accepted', }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Policy.objects.count(), 1)
+        policy = Policy.objects.get()
+        self.assertEqual(policy.customer_id, 1)
+        self.assertEqual(policy.policy_type, Policy.PolicyTypes.personal_accident)
+        self.assertEqual(policy.state, Policy.StateChoices.accepted)
+        self.assertEqual(
+            PolicyHistory.objects.filter(policy=policy, action=PolicyHistory.ActionTypes.creation).count(), 1
+        )
+        self.assertEqual(
+            PolicyHistory.objects.filter(policy=policy, action=PolicyHistory.ActionTypes.quotation).count(), 1
+        )
+
+    def test_policy_activation(self):
+        """
+        Test quote getting accepted.
+        """
+        url = '/api/v1/quote/'
+        admin_user = User.objects.get(id=1)
+        self.client.force_login(admin_user)
+        quote = Policy(
+            policy_type=Policy.PolicyTypes.personal_accident,
+            customer=admin_user
+        )
+        quote.save()
+        response = self.client.post(url, {'quote_id': 1, 'status': 'accepted'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = self.client.post(url, {'quote_id': 1, 'status': 'active'}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(Policy.objects.count(), 1)
+        policy = Policy.objects.get()
+        self.assertEqual(policy.customer_id, 1)
+        self.assertEqual(policy.policy_type, Policy.PolicyTypes.personal_accident)
+        self.assertEqual(policy.state, Policy.StateChoices.active)
+        self.assertEqual(
+            PolicyHistory.objects.filter(policy=policy, action=PolicyHistory.ActionTypes.creation).count(), 1
+        )
+        self.assertEqual(
+            PolicyHistory.objects.filter(policy=policy, action=PolicyHistory.ActionTypes.quotation).count(), 1
+        )
+        self.assertEqual(
+            PolicyHistory.objects.filter(policy=policy, action=PolicyHistory.ActionTypes.activation).count(), 1
         )
