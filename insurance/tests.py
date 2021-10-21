50e01fa63ed4c0dba6b6516bfe3355d978ae01e3
Seyed Mohammad Hosseini <seyed_50005@yahoo.com>
2021-10-21 13:16:36 +0330

    feat: add quote creation test

--- insurance/tests.py
+++ insurance/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from users.models import User
+from insurance.models import Policy, PolicyHistory
+
+
+class PolicyProcessTests(APITestCase):
+    fixtures = ["admin_user.json"]
+
+    def test_quote_creation(self):
+        """
+        Test quote creation.
+        """
+        url = '/api/v1/quote/'
+        data = {
+            'customer_id': 1,
+            'type': 'personal-accident',
+        }
+        admin_user = User.objects.get(id=1)
+        self.client.force_login(admin_user)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Policy.objects.count(), 1)  # admin user already exists
+        policy = Policy.objects.get()
+        self.assertEqual(policy.customer_id, 1)
+        self.assertEqual(policy.policy_type, Policy.PolicyTypes.personal_accident)
+        self.assertEqual(policy.state, Policy.StateChoices.new)
+        self.assertEqual(
+            PolicyHistory.objects.filter(policy=policy, action=PolicyHistory.ActionTypes.creation).count(), 1
+        )
